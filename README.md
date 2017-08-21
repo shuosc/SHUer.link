a79e2c8f3735750653efee78c764b14c214fe9d1
cosformula <cosformula@qq.com>
2017-08-21 15:16:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# SHUer.link
+
+> Inspired by BYR.wiki
+
+使用Nuxtjs构建的校园导航网站。
+
+# 开发
+
+## 安装依赖
+
+`yarn install` 或者 `npm install`
+
+## 测试服务器
+
+`npm run dev`
+启动测试服务器后可通过`localhost:3000`访问
+
+## 静态化
+
+`npm run generate`
+
+# 完成度
+
+* <input type="checkbox" value=""/>首页布局
+* <input type="checkbox" value=""/>首页背景
+* <input type="checkbox" value=""/>搜索框
+* <input type="checkbox" value=""/>导航网站
+* <input type="checkbox" value=""/>登陆／设置同步
+* <input type="checkbox" value=""/>chrome插件
+
+# 依赖于
+
+* `purecss`
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        SHUer.link
+      </h1>
+      <form class="pure-form">
+        <input class="pure-input-1" type="text" placeholder="search"><br>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../node_modules/purecss/build/pure.css'
+.container
+{
+  /* min-height: 100vh; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title
+{
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 5rem;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links
+{
+  padding-top: 15px;
+}
+</style>
